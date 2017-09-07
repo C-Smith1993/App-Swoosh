4dad44d8069de852d8c92799ff99bb87072013fd
C-Smith1993 <chrissmithreds93@hotmail.com>
2017-09-07 14:14:29 +0100

    Added code so that the player has to select an option before they can tap the next button.

---
+++ App-Swoosh/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  App-Swoosh
+//
+//  Created by Chris Smith on 07/09/2017.
+//  Copyright © 2017 CDSApps. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+    
+}
