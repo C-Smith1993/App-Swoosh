d7b634f598a5d16aa460aa19d836055b77a369ec
C-Smith1993 <chrissmithreds93@hotmail.com>
2017-09-08 16:52:27 +0100

    Application is now complete.
    Able to send data between view controllers and the UI fits multiple iPhone screen sizes.

--- App-Swoosh/Player.swift
+++ App-Swoosh/Player.swift
@@ -10,8 +10,7 @@ import Foundation
 
 struct Player{
     
-    var desiredLeague: String?
-    var selectedSkillLevel: String?
-    
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
     
 }
